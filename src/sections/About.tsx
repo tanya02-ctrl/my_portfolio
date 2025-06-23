dfa037ea5acfd2f88a355916034476af4d6efb19
Tanya Shree <itstanyashree.com>
2025-06-23 22:34:13 +0530

    New update

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -38,8 +38,7 @@ export default function About() {
             </a>
           </div>
 
-          {/* Resume Button */}
-          <a href="/public/TanyaShree_Resume.pdf" download
+          <a href="/TanyaShree_Resume.pdf" download
            className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg font-medium hover:bg-blue-700 transition-colors">
            Resume
           </a>
